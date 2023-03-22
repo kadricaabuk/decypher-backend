4f3d84373d5d2a4a7b6910c6a50cb1db00e7b5fe
kadricaabuk <kadricabuk@gmail.com>
2023-03-22 23:35:54 +0300

    login api created

--- src/controllers/User/UserController.js
+++ src/controllers/User/UserController.js
@@ -1,20 +1,80 @@
 import { handleResponse } from "../globalFunctions.js";
-import { getAll, saveUser } from "./UserFunctions.js";
+import { getAll, saveUser, loginUser, getUserByEmail } from "./UserFunctions.js";
 class UserController {
     static async main(req, res){
         if(req.method == "GET"){
             const users = await getAll()
-            const response = handleResponse(users)
+            let response = {}
+            if(users.length){
+                response = handleResponse({
+                    response: users
+                })
+            }else{
+                response = handleResponse({
+                    response: [],
+                    statusCode: 204,
+                    success: false,
+                    message: "Users are not found!"
+                })
+            }
             res.send(response);
         }else{
             const {body} = req
             const user = await saveUser(body)
-            const response = handleResponse(user)
+            let response = {}
+            if(user.name){
+                response = handleResponse({
+                    statusCode: 201,
+                    response: user,
+                    message: "User created, successfully!"
+                })
+            }else {
+                response = handleResponse({
+                    response: [],
+                    statusCode: 400,
+                    success: false,
+                    message: "User can not created!"
+                })
+            }
             res.send(response)
         }
     }
-    static home(req, res){
-        res.send('Hello from home of main controller');
+    static async getByEmail(req, res){
+        const {email} = req.query;
+        const user = await getUserByEmail(email);
+        let response = {}
+        if(user.length){
+            response = handleResponse({
+                response: user
+            })
+        }else{
+            response = handleResponse({
+                response: [],
+                statusCode: 204,
+                success: false,
+                message: "User is not found!"
+            })
+        }
+        res.send(response)
+    }
+    static async login(req, res){
+        const {body} = req
+        const user = await loginUser(body)
+        let response = {}
+        if(user.length){
+            response = handleResponse({
+                response: user,
+                message: "Login successfull!"
+            })
+        }else{
+            response = handleResponse({
+                response: [],
+                statusCode: 204,
+                success: false,
+                message: "User is not found!"
+            })
+        }
+        res.send(response)
     }
 }
 
--- src/routes/user.js
+++ src/routes/user.js
@@ -2,9 +2,14 @@ import {Router} from 'express'
 import {UserController} from '../controllers/index.js';
 import {Auth} from '../middleware/index.js';
 const UserRoute = Router()
+const {main, login, getByEmail} = UserController
+const handleRouteString = (route) => route ? `/user/${route}` : '/user';
+
 
 UserRoute.use(Auth.validate)
-UserRoute.get('/user', UserController.main)
-UserRoute.post('/user', UserController.main)
+UserRoute.get(handleRouteString(), main)
+UserRoute.post(handleRouteString(), main)
+UserRoute.post(handleRouteString('login'), login)
+UserRoute.get(handleRouteString('query'), getByEmail)
 
 export default UserRoute
